3ae3073cf4052be0e910e83852b03ed374fa685e
张世虎 <zhangshihu@zhangshihu.local>
2024-03-15 17:30:04 +0800

    华为云 functions 支持 obs 触发，加入环境变量 DATAKIT_IP、DATAKIT_PORT 具体可看文档

--- Huaweicloud/Functiongraph/functiongraph_forward.py
+++ Huaweicloud/Functiongraph/functiongraph_forward.py
@@ -1,54 +1,127 @@
-# -*- coding:utf-8 -*-
 import time
 import json
 import urllib3
 import base64
-import gzip
+import os
+import urllib
+import obs
 
 
-DATAKIT = 'http://xx.xx.xx.xx'
+class MyEncoder(json.JSONEncoder):
 
-print('Start')
+    def default(self, obj):
+        """
+        判断是否为bytes类型的数据是的话转换成str
+        :param obj:
+        :return:
+        """
+        if isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        return json.JSONEncoder.default(self, obj)
+
+
+def get_env_var(envvar, default, boolean=False):
+    """
+    读取环境变量
+    """
+    value = os.getenv(envvar, default=default)
+    if boolean:
+        value = value.lower() == "true"
+    return value
+
+
+DATAKIT_IP = get_env_var("DATAKIT_IP", "")
+DATAKIT_PORT = get_env_var("DATAKIT_PORT", 9529)
+
 
 def push_dk(data):
     http = urllib3.PoolManager()
     data = json.dumps(data)
-    response = http.request("POST", f'{DATAKIT}:9529/v1/write/logging', body=data, headers={'Content-Type': 'application/json'})
-    print('dk_code:', response.status)
+    response = http.request("POST", f'{DATAKIT_IP}:{DATAKIT_PORT}/v1/write/logging', body=data,
+                            headers={'Content-Type': 'application/json'})
+    print('dk_response_code:', response.status)
 
-def to_datakit_data(event):
-    
+
+def to_datakit_data(event, event_type):
+    tags = {}
+    for k, v in event.items():
+        if k in ['time', 'message']:
+            continue
+        elif isinstance(v, bytes):
+            tags[k] = v.decode()
+            continue
+
+        tags[k] = json.dumps(v)
     data = {
-        'measurement': 'function_graph_forwarder',
-        'time'  : round(time.time()),
-        'tags'  : {
-            'log_uid':event.get('log_uid'),
-            'timestamp':str(event.get('timestamp', round(time.time())))
-        },
-        'fields' : {'message':json.dumps(event)},
+        'measurement': event_type,
+        'time': round(time.time()),
+        'tags': tags,
+        'fields': {'message': json.dumps(event, ensure_ascii=False, cls=MyEncoder)},
     }
     return data
 
-def event_encode(event):
+
+def s3_handler(event, context):
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"])
+    region = event['Records'][0]['awsRegion']
+
+    access_key_id = context.getSecurityAccessKey()
+    secret_access_key = context.getSecuritySecretKey()
+    secret_token = context.getSecurityToken()
+
+    client = obs.ObsClient(access_key_id=access_key_id, secret_access_key=secret_access_key,
+                           server=f'https://obs.{region}.myhuaweicloud.com', security_token=secret_token)
+    response = client.getObject(bucketName=bucket, objectKey=key, loadStreamInMemory=True)
+    body = response["body"]
+
+    return body
+
+
+def lts_handler(event, context):
     encoding_data = event["lts"]["data"]
-    data = base64.b64decode(encoding_data.encode('utf-8')) 
+    data = base64.b64decode(encoding_data.encode('utf-8'))
     text = json.loads(data)
-    return text
+    logs = json.loads(text.get('logs'))
+    return logs
+
+
+def parse_event_type(event):
+    """
+    判断日志类型
+    """
+    if "Records" in event and len(event["Records"]) > 0:
+        if "s3" in event["Records"][0]:
+            return "obs"
+
+    elif "lts" in event:
+        return "lts"
+    raise Exception("Event type not supported (see #Event supported section)")
+
 
 def handler(event, context):
+    event_type = "function_graph_forwarder"
     try:
-        event_list = event_encode(event).get('logs')
-    except:
-        event_list = [{'message':event}]
-        print('eventbridge event')
-    
-    event_list = json.loads(event_list)
-    print('event_list_value', len(event_list))
-    dk_data_list = []
-    for eventdata in event_list:
-        data = to_datakit_data(eventdata)
-        dk_data_list.append(data)
-    print('dk_data_list',dk_data_list)
-    push_dk(dk_data_list) #上报给第三方数据
+        # 找到对应的解析器
+        event_type = parse_event_type(event)
+        if event_type == "obs":
+            events = s3_handler(event, context)
+        elif event_type == "lts":
+            events = lts_handler(event, context)
+        else:
+            events = [{'message': event}]
 
-    return
+    except Exception as e:
+        err_message = "Error parsing the object. Exception: {} for event {}".format(
+            str(e), event
+        )
+        events = [err_message]
+
+    if isinstance(events, dict):
+        events = [events]
+
+    dk_data_list = []
+    for event_data in events:
+        data = to_datakit_data(event_data, event_type)
+        dk_data_list.append(data)
+    push_dk(dk_data_list)  # 上报给第三方数据
--- Huaweicloud/Functiongraph/huaweicloud-functiongraph-forward-introduce.md
+++ Huaweicloud/Functiongraph/huaweicloud-functiongraph-forward-introduce.md
@@ -1,10 +1,11 @@
 # 采集器「HuaweiCloud-FunctionGraph」配置手册
 
-通过 华为云 中的 FunctionGraph 对 华为云 中的 LTS 日志数据进行抓取并上报到观测云日志中。
+通过 华为云 中的 FunctionGraph 对 华为云 中的 LTS、OBS 日志数据进行抓取并上报到观测云日志中。
 
 ## 配置 FunctionGraph
 
-### 使用控制台创建 FunctionGraph 函数
+### 使用控制台创建 FunctionGraph 函数，例如触发器为 LTS
+
 
 1.打开 FunctionGraph 控制台的函数页面。
 
@@ -20,15 +21,13 @@
 
 7.点击创建函数
 
-8.在 GitHub 中拉取同步代码至下方代码源中，并添加DATAKIT为datakit部署地址，点击`Depoly`
+8.在 GitHub 中拉取同步代码至下方代码源中，并添加环境变量 DATAKIT_IP 为 datakit 部署地址，如果端口有改变，可加入环境变量 DATAKIT_PORT（默认 9529 ） 为对应的 IP，点击`部署`
 
-### 配置 Lambda 触发器
+9.点击 `添加触发器`
 
-1.点击 `添加触发器`
+10.设置`触发器类型`为`云日志服务 (LTS)` 
 
-2.设置`触发器类型`为`云日志服务 (LTS)` 
-
-3.选择需要监听的`日志组`，点击添加。
+11.选择需要监听的`日志组`，点击添加。
 
 ## X. 附录
 
@@ -40,4 +39,10 @@
 lts:*:get
 lts:*:list
 functiongraph:*:*
+```
+有权在当前区域和 华为云 账户中创建 LTS 日志组，以及创建日志流并将事件放入这些流中，所需权限列表
+
+```
+functiongraph:*:*
+obs:object:GetObject"
 ```
